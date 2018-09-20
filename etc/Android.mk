d79e4818518559fc83d2dd106a6421522daa31c9
Shinru Han <shinruhan@google.com>
2018-09-20 12:00:45 +0800

    gps: Avoid duplicate gps.conf
    Copy gps.conf file only when BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET
    is not set
    
    Change-Id: Id746f9946076e3a81818c0f36cbef0bdbe84fbf5

--- etc/Android.mk
+++ etc/Android.mk
@@ -1,4 +1,4 @@
-
+ifneq ($(BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET),true)
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -9,4 +9,5 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/etc/
 LOCAL_SRC_FILES := gps.conf
 
 include $(BUILD_PREBUILT)
+endif
 
